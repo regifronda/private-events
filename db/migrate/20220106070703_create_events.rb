5d8c2a8304583445c8f3c4dc5ce46d029a404d72
Regi Fronda <regisfronda@gmail.com>
2022-01-05 23:17:45 -0800

    add columns to migration

--- db/migrate/20220106070703_create_events.rb
+++ db/migrate/20220106070703_create_events.rb
@@ -1,7 +1,11 @@
 class CreateEvents < ActiveRecord::Migration[6.1]
   def change
     create_table :events do |t|
-
+      t.string :name
+      t.text :description
+      t.date :date
+      t.string :location
+      
       t.timestamps
     end
   end
