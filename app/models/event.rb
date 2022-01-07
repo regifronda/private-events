c865ab3caea4315b204fb30ac10a3cc9ff8fa531
Regi Fronda <regisfronda@gmail.com>
2022-01-07 12:19:18 -0800

    Change foreign key for creator to user_id

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ApplicationRecord
   :date
-  belongs_to :creator, foreign_key: "creator_id", class_name: "User"
+  belongs_to :creator, foreign_key: "user_id", class_name: "User"
 end
