e6554cce0a288359c8c854b8227b567fc5983b08
Regi Fronda <regisfronda@gmail.com>
2022-01-07 02:27:20 -0800

    Add association between event creator and event

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { in: 2..20 }
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 6..20 }
+  has_many :created_events, class_name: "Event"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
