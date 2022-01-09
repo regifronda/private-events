00f95a744789ef5c1f7d4fe896f205daa182700a
Regi Fronda <regisfronda@gmail.com>
2022-01-09 15:21:08 -0800

    Add #new and #create actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,38 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
     @events = Event.all.order("created_at DESC")
+  end
+
+  def new
     @event = Event.new
+  end
+
+  def create
+    @event = current_user.build_event(event_params)
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to root_path, notice: "Event was successfully created." }
+        format.json { render :show, status: :created, location: @event }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section."
+      redirect_to root_url
+    end
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :description, :date, :location)
   end
 end
