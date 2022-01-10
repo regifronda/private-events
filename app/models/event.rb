6092d28b2ad1c2bf347f6c3992aaa8b9ce24796c
Regi Fronda <regisfronda@gmail.com>
2022-01-09 16:14:50 -0800

    Add validations for name, description, date, and location

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
-  :date
+  validates :name, presence: true, length: { maximum: 64 }
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :date, presence: true
+  validates :location, presence: true
   belongs_to :creator, foreign_key: "user_id", class_name: "User"
 end
