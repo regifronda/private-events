0d9bc1c120ccb6db939301144cb109773929c602
Regi Fronda <regisfronda@gmail.com>
2022-01-11 11:53:20 -0800

    Add route for event's #show action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create, :show, :index]
-  resources :events
+  resources :events, only: [:index, :new, :create, :show]
+  get "/events/:id", to: "events#show"
   root "events#index"
 end
