982b7af1f7f64e03cb6bb07e6feb9a59ce2a1dbf
Regi Fronda <regisfronda@gmail.com>
2022-01-24 17:23:41 -0800

    Add foreign key and columns for attendances table

--- db/migrate/20220113192513_create_attendances.rb
+++ db/migrate/20220113192513_create_attendances.rb
@@ -1,6 +1,8 @@
 class CreateAttendances < ActiveRecord::Migration[6.1]
   def change
     create_table :attendances do |t|
+      t.belongs_to :event, foreign_key: true
+      t.belongs_to :physician, foreign_key: true
 
       t.timestamps
     end
