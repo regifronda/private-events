ebe68cd1910fe9ecbdafebe29fd8e7e3577239f8
Regi Fronda <regisfronda@gmail.com>
2022-02-02 12:15:53 -0800

    Add changes so show action finds the current user who created events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user_events = current_user.events.all
+    @user_events = User.find(params[:id]).events.all
     @user = User.find(params[:id])
   end
-
-  
 end
